59835a3a955c79c3d4148d17eba29771689b823e
Mark.Adelhelm <Mark.Adelhelm@gmx.com>
2018-04-08 22:42:35 +0200

    First Step?!

---
+++ Maskoram/Maskoram.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maskoram.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Maskoram/Maskoram/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Maskoram.Startup))]
+namespace Maskoram
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
